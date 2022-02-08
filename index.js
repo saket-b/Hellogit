72573fd36a81590084f847ba3dcacc765d147b09
saket-b <saketbharti201721@gmail.com>
2022-02-08 23:06:49 +0530

    make javascript

---
+++ index.js
@@ -0,0 +1,38 @@
+
+var a;
+a = 10;
+console.log(a);
+console.log(typeof a);
+
+
+a = "saket";
+console.log(a);
+
+a = null;
+console.log(a);
+
+a = 2.54
+console.log(a);
+console.log("hi hello");
+
+
+// Boolean 
+
+var t = true;
+var f = false;
+
+console.log(t);
+console.log(f);
+
+// '',          "",             ``
+//single quotes, double quotes, backstick
+var str = 'a'
+console.log(typeof str);
+
+str = "saket"
+console.log(str);
+
+
+
+
+
